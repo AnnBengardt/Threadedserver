8d642fa4a8279de5e18b8c41c92fe661f269a6aa
AnnBengardt <marunko.a@yandex.ru>
2021-04-07 13:14:36 +0300

    Added port scanner

--- port_scanner.py
+++ port_scanner.py
@@ -1,26 +1,52 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import socket
-from threading import Thread
+import threading
+import sys
 
-N = 2**16 - 1
+global socks_list
 
-host = str(input('Введите имя хоста или IP-адрес\n'))
+def progress_bar(step, n):
+    len = 60
+    len_filled = int(round(len * step/n))
+    len_unfilled = len - len_filled
+    bar = "|"*len_filled+"-"*len_unfilled
+    percent = round(100.0 * step/n, 1)
+    if step == n - 1:
+        dop = '\n'
+    else:
+        dop = '\r'
+    sys.stdout.write(f'[{bar}] {percent}% {dop}')
+    sys.stdout.flush()
 
-try:
-    sock = socket.socket()
-    sock.connect((host, 9999))
-    sock.close()
-except (socket.gaierror):
-    print(host, ' - unknown host')
-else:
-    for port in range(1, N):
+
+
+class MyThread(threading.Thread):
+    def __init__(self, host, n):
+        super(self, name="thread" + str(n))
+        self.host = host
+        self.port = n
+
+    def run(self):
         sock = socket.socket()
         try:
-            sock.connect((host, port))
-            print("Порт ", port, " открыт")
+            sock.connect((self.host, self.port))
+            with lock:
+                socks_list.append(self.port)
         except:
-            continue
-        finally:
-		sock.close()
+            pass
+        sock.close()
+
+lock = threading.Lock()
+n = 2**16 - 1
+
+host = input('Type in the host:')
+socks_list = []
+threads = [MyThread(host, port) for port in range(1, n)]
+for port in range(1, n):
+    threads[port - 1].start()
+    progress_bar(port, n)
+[threads[port - 1].join() for port in range(1, n)]
+socks_list.sort()
+for port in socks_list:
+    print("Port#", port, " is opened")
